dfa4008be501e51f90f096d815a820b20c0e706b
Thomas Tranchet <toto.tranchet@gmail.com>
2021-01-11 18:15:57 +0100

    Il y a toujours des pb

--- app/main.py
+++ app/main.py
@@ -1,130 +1,199 @@
-import fastapi
-import mysql.connector
-
-
-mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="root",
-  password="molomo2404",
-    database = "SD_Wan"
-)
-
-print(mydb)
-
-from fastapi import FastAPI
-from fastapi.requests import Request
+from fastapi import FastAPI 
+from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
-from typing import Optional
 import codecs
-import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+import numpy as np 
+import pulp 
+from fastapi.staticfiles import StaticFiles
+import io
+from starlette.responses import StreamingResponse
 
 
-def actions_RL():
-    return "hello"
-
-
-test = actions_RL()
 app = FastAPI()
-file = codecs.open("Index.html", "r")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+db = []
+path = []
 
 
-@app.get("/", response_class=HTMLResponse)
-def Docu():
-    docuStr = "Voici la liste des features : GenerateOnosStruc(Param1: int, Param2: int, Param3: int, Param4: int) that generate Onos struc"
+@app.get('/', response_class=HTMLResponse)
+async def index():
+    file = codecs.open("static/index.html", "r")
+    return file.read()
 
+@app.get('/graph')
+async def get_graph():
+    return db
+
+@app.get("/vector_image")
+def image_endpoint():
+    file_like = open("images/original.jpg", mode="rb")
+    return StreamingResponse(file_like, media_type="image/jpg")
+
+@app.get("/opti_image")
+def image_endpoint():
+    file_like = open("images/shortest_path.png", mode="rb")
+    return StreamingResponse(file_like, media_type="image/jpg")
+    
+@app.get('/generate/{nodes_edges}', response_class=HTMLResponse)
+def image_nertworkx(nodes:int = 30, edges:int  = 3):
+    g = nx.to_directed(nx.barabasi_albert_graph(nodes, edges))
+    nx.draw(g, with_labels=True)
+    plt.savefig("images/original.jpg")
+
+    dict_capa = {}
+    for i, j in g.edges:
+        dict_capa[i, j] = dict_capa[j, i] = round(random.uniform(1.0, 20.0), 0)
+    
+    nx.set_edge_attributes(g, dict_capa, 'capacity')
+    
+    dict_used = {}
+    for i, j in g.edges:
+        dict_used[i, j] = dict_used[j, i] = min(round(random.uniform(0.0, 15.0), 0),dict_capa[i, j])
+    
+    nx.set_edge_attributes(g, dict_used, 'used')
+    
+    dict_ratio = {}
+    for i, j in g.edges:
+        dict_ratio[i, j] = dict_ratio[j, i] = dict_used[i, j]/dict_capa[i, j]
+    
+    nx.set_edge_attributes(g, dict_ratio, 'ratio')
+    
+    dict_delay = {}
+    for i, j in g.edges:
+        dict_delay[i, j] = dict_delay[j, i] = round(random.uniform(1.0, 20.0), 2)
+    
+    nx.set_edge_attributes(g, dict_delay, 'delay')
+
+    i,j = zip(*g.edges)
+    unique_i = np.unique(i)
+    list_ratio = []
+    for u_i in unique_i:
+        temp_dict = {}
+        for i, j in g.edges:
+            if u_i==i:
+                temp_dict["i"] = i
+                temp_dict["j"] = j
+                temp_dict["ratio"] = dict_ratio[i,j]
+                list_ratio.append(*zip([list(temp_dict.values())[2]],[list(temp_dict.values())[0]],[list(temp_dict.values())[1]]))
+
+    list_ratio_i = (sorted(list_ratio, key=lambda element: (element[1],element[0])))
+    list_ratio_j = (sorted(list_ratio, key=lambda element: (element[2],element[0])))
+
+    dict_score = {}
+    score = 1
+    prev_ratio = -1
+    prev_i = 0
+    for ratio,i,j in list_ratio_i:
+        if ratio != prev_ratio:
+            score += 1
+        prev_ratio = ratio
+        if (prev_i != i):
+            score = 0
+            prev_i = i
+        dict_score[i, j] = score
+
+    score = 1
+    prev_ratio = -1
+    prev_j = 0
+    for ratio,i,j in list_ratio_j:
+        if ratio != prev_ratio:
+            score += 1
+        prev_ratio = ratio
+        if (prev_j != j):
+            score = 0
+            prev_j = j
+        dict_score[j, i] = score
+
+    nx.set_edge_attributes(g, dict_score, 'score')
+
+    if len(db) > 0 :
+        db.pop(0)
+    db.append(nx.to_dict_of_dicts(g))
+    
+    file = codecs.open("static/graph_view.html", "r")
     return file.read()
 
 
-@app.get("/GenerateOnosStruc")
-def GenerateOnosStruc():
-    print(test)
-    return {"message": "World" + test}
+@app.get('/generate_path/{source_target}',response_class=HTMLResponse)
+async def opti_path(source:int = 0, target:int  = 10):
+    g = nx.Graph(db[0])
+    list_keys = ['shortest_path','min_delay','min_banwidth_sum','min_banwidth_square_sum','min_score'] #,'min_square_score'
+    dict_prob = {}
+    dict_prob = dict_prob.fromkeys(list_keys)
 
-"""
-mycursor = mydb.cursor()
+    opti_path = {}
+    opti_path = dict([(key, []) for key in list_keys])
 
-sql = "INSERT INTO Graph_Onos (address) VALUES (%s, %s)"
-val = ("John", "Highway 21")
-mycursor.execute(sql, val)
-"""
+    # binary variable to state a link is chosen or not
+    for keys,prob in dict_prob.items():
+        prob = pulp.LpProblem("%s" % keys, pulp.LpMinimize)
+        var_dict = {}
+        for (i, j) in g.edges:
+            x = pulp.LpVariable("%s_(%s_%s)" % (keys,i,j), cat=pulp.LpBinary)
+            var_dict[i, j] = x
+        bdw = 1
+        
+        # objective function
+        if keys == "shortest_path":
+            prob += pulp.lpSum(var_dict[i, j] for i, j in g.edges), "Sum Node Count"
+        elif keys == "min_delay":
+            prob += pulp.lpSum([g.edges[i,j]['delay'] * var_dict[i, j] for i, j in g.edges]), "Sum delay"
+        elif keys == "min_banwidth_sum":
+            prob += pulp.lpSum([g.edges[i,j]['ratio'] * var_dict[i, j] for i, j in g.edges]), "Sum bandwidth ratio"
+        elif keys == "min_banwidth_square_sum":
+            prob += pulp.lpSum([g.edges[i,j]['ratio'] ** 20 * var_dict[i, j] for i, j in g.edges]), "Sum square bandwidth ratio"
+        elif keys == "min_score":
+            prob += pulp.lpSum([g.edges[i,j]['score'] * var_dict[i, j] for i, j in g.edges]), "Sum score"
+#        elif keys == "min_square_score":
+#            prob += pulp.lpSum([(g.edges[i,j]['score'] ** 20 * var_dict[i, j]) for i, j in g.edges]), "Sum square score"
+            
+        # constraints
+        for node in g.nodes:
+            rhs = 0
+            if node == source:
+                rhs = -1
+            elif node == target:
+                rhs = 1
+            prob += pulp.lpSum([var_dict[i, k] for i, k in g.edges if k == node]) - pulp.lpSum([var_dict[k, j] for k, j in g.edges if k == node]) == rhs
+        
+        # constraints on capacity
+        for i,k in g.edges:
+            prob += var_dict[i, k]*bdw + g.edges[i,k]['used']  <=g.edges[i,k]['capacity']
 
+        # solve
+        prob.solve()
+        print("\n\n" + str(keys))
+        print(pulp.LpStatus[prob.status])
+        print(pulp.value(prob.objective))
+        for link in g.edges:
+            if var_dict[link].value() == 1.0:
+                print(link, end=" , ")
+                opti_path[keys].append(link)
 
-# mydb.commit()
+    for key,path in opti_path.items():
+        res = [[i for i, j in path], 
+        [j for i, j in path]] 
+        un_res = list()
+        for i in res[0]:
+            un_res.append(i)
+        for i in res[1]:
+            if i not in un_res:
+                un_res.append(i) 
 
-def convertToBinaryData(filename):
-    # Convert digital data to binary format
-    with open(filename, 'rb') as file:
-        binaryData = file.read()
-        print(binaryData)
-    return binaryData
+    print(un_res)
+    sg = g.subgraph(un_res)
+    plt.close()
+    nx.draw(sg, with_labels=True)
+    plt.savefig("images/{}".format(keys))
+    plt.show()
 
-def insertBLOB(photo):
-    print("Inserting BLOB into python_employee table")
-    try:
-        connection = mysql.connector.connect(host="127.0.0.1",
-                                             user="root",
-                                             password="molomo2404",
-                                             database = "SD_Wan")
-
-        cursor = connection.cursor()
-        sql_insert_blob_query = """INSERT INTO Graph_Onos (Graph) VALUES (%s)"""
-
-        empPicture = convertToBinaryData(photo)
-        # empPicture = photo
-        # Convert data into tuple format
-        insert_blob_tuple = (empPicture,)
-        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
-        connection.commit()
-        print("Image and file inserted successfully as a BLOB into python_employee table", result)
-
-    except mysql.connector.Error as error:
-        print("Failed inserting BLOB data into MySQL table {}".format(error))
-
-    finally:
-        if (connection.is_connected()):
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
-
-insertBLOB("/Users/tomwilliams/Downloads/minion.jpg")
-# insertBLOB("D:\Python\Articles\my_SQL\images\scott_photo.png")
-
-
-
-
-
-
-def write_file(data, filename):
-    # Convert binary data to proper format and write it on Hard Disk
-    with open(filename, 'wb') as file:
-        file.write(data)
-
-def readBLOB(id, photo):
-    print("Reading BLOB data from python_employee table")
-
-    try:
-        connection = mysql.connector.connect(host="127.0.0.1",
-                                             user="root",
-                                             password="molomo2404",
-                                             database = "SD_Wan")
-
-        cursor = connection.cursor()
-        sql_fetch_blob_query = """SELECT * from Graph_Onos where Num_Graph = %s"""
-        print(sql_fetch_blob_query)
-        cursor.execute(sql_fetch_blob_query, (id,))
-        record = cursor.fetchall()
-        for row in record:
-            print("Id = ", row[0], )
-            image = row[1]
-            print("Storing employee image and bio-data on disk \n")
-            write_file(image, photo)
-    except mysql.connector.Error as error:
-        print("Failed to read BLOB data from MySQL table {}".format(error))
-
-    finally:
-        if (connection.is_connected()):
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
-
-readBLOB(5, "/Users/tomwilliams/OneDrive - ESME/Esme_inge_3/Majeur_IA/SD_WAN/minion.jpg")
+    if pulp.LpStatus[prob.status] != 'Infeasible'  :
+        file = codecs.open("static/path_view.html", "r")
+        return file.read()
+    else: return ('Infeasible')
